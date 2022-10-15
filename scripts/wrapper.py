b22125b839a0f2b105c391792b8715fe63415123
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-15 18:04:58 -0400

    refactoring wrapper for class based usage

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -6,12 +6,9 @@ Created on Fri Oct 14 19:04:47 2022
 """
 import requests
 import json
-
-
 from web3 import Web3
-
 # Change this to use your own RPC URL
-web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth_goerli'))
+web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 # AggregatorV3Interface ABI
 abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
 # Price Feed address
@@ -26,3 +23,22 @@ validRoundId = 18446744073709554177
 
 historicalData = contract.functions.getRoundData(validRoundId).call()
 print(historicalData)
+
+class OracleClient:
+    def __init__(self, address):
+        self.address = address
+    
+    def live_price_data(self):
+        web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+        # AggregatorV3Interface ABI
+        abi = '[{ "inputs": [ { "internalType": "address", "name": "_ETHConverterAddress", "type": "address" }, { "internalType": "address", "name": "_asset", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "ETHConverterAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "asset", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getLatestPrice", "outputs": [ { "internalType": "int256", "name": "", "type": "int256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "convertETHPriceToUSD", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "viewAssetAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }]' 
+        
+        # Set up contract instance
+        contract = web3.eth.contract(address=self.address, abi=abi)
+
+        #  Valid roundId must be known. They are NOT incremental.
+        # invalidRoundId = 18446744073709562300
+        validRoundId = 18446744073709554177
+
+        historicalData = contract.functions.getRoundData(validRoundId).call()
+        print(historicalData)
