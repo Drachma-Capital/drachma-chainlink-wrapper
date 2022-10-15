36a527530dbd8c9c20cb8adb82b7a20fc707cb0f
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-15 18:17:51 -0400

    finished wrapper

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -11,7 +11,7 @@ class OracleClient:
     def __init__(self, address):
         self.address = address
     
-    def live_price_in_eth(self):
+    def price_eth_pair(self):
         web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
         # AggregatorV3Interface ABI
         abi = '[{ "inputs": [ { "internalType": "address", "name": "_ETHConverterAddress", "type": "address" }, { "internalType": "address", "name": "_asset", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "ETHConverterAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "asset", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getLatestPrice", "outputs": [ { "internalType": "int256", "name": "", "type": "int256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "convertETHPriceToUSD", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "viewAssetAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }]' 
@@ -23,7 +23,7 @@ class OracleClient:
         price = (current_price)/10**18
         return price
     
-    def live_price_in_usd(self):
+    def price_eth_pair_to_usd(self):
         web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
         # AggregatorV3Interface ABI
         abi = '[{ "inputs": [ { "internalType": "address", "name": "_ETHConverterAddress", "type": "address" }, { "internalType": "address", "name": "_asset", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "ETHConverterAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "asset", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getLatestPrice", "outputs": [ { "internalType": "int256", "name": "", "type": "int256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "convertETHPriceToUSD", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "viewAssetAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }]' 
@@ -32,5 +32,17 @@ class OracleClient:
         contract = web3.eth.contract(address=self.address, abi=abi)
         
         current_price = contract.functions.convertETHPriceToUSD().call()
-        price = (current_price)/10**18
+        price = (current_price)/10**26
+        return price
+   
+    def price_usd_pair(self):
+        web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+        # AggregatorV3Interface ABI
+        abi = '[{ "inputs": [ { "internalType": "address", "name": "_ETHConverterAddress", "type": "address" }, { "internalType": "address", "name": "_asset", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "ETHConverterAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "asset", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getLatestPrice", "outputs": [ { "internalType": "int256", "name": "", "type": "int256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "convertETHPriceToUSD", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "viewAssetAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }]' 
+        
+        # Set up contract instance
+        contract = web3.eth.contract(address=self.address, abi=abi)
+        
+        current_price = contract.functions.getLatestPrice().call()
+        price = (current_price)/10**8
         return price
