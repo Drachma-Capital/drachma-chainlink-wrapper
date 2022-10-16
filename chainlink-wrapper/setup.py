78227160654de8e1e61171cf53c399ba9020d19b
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-16 10:52:25 -0400

    added setup.py

---
+++ chainlink-wrapper/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='chainlink-wrapper',
+    version='1.0',    
+    description='A wrapper to get live price data from chainlink oracles on-chain',
+    url='https://github.com/Drachma-Capital/drachma-chainlink-wrapper',
+    author='Joshua Finkelstein',
+    author_email='joshfinkelstein3@gmail.com',
+    license='BSD 2-clause',
+    packages=['chainlink-wrapper'],
+    install_requires=[
+                      'web3',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
