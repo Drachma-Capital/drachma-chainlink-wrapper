eadcde7f0b73790a83046139d658cc0b45ccad7a
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-20 09:45:27 -0400

    naming conventions and added flattend source code for easy verifying

--- chainlink_wrapper/rpc.py
+++ chainlink_wrapper/rpc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-goerli = "https://goerli.infura.io/v3/f4604f2f3c7540929af83597010178cd"
-ethereum = "https://mainnet.infura.io/v3/f4604f2f3c7540929af83597010178cd"
-moonriver = "https://rpc.api.moonriver.moonbeam.network"
-arbitrum = "https://rpc.ankr.com/arbitrum"
-optimism = "https://mainnet.optimism.io"
+goerli_rpc = "https://goerli.infura.io/v3/f4604f2f3c7540929af83597010178cd"
+ethereum_rpc = "https://mainnet.infura.io/v3/f4604f2f3c7540929af83597010178cd"
+moonriver_rpc = "https://rpc.api.moonriver.moonbeam.network"
+arbitrum_rpc = "https://rpc.ankr.com/arbitrum"
+optimism_rpc = "https://mainnet.optimism.io"
 
