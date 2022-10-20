66c6e1110336eb6e1751b7a7bfb0e70712dcb45d
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-20 09:14:31 -0400

    added token pairings

---
+++ chainlink_wrapper/token_pairs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 08:58:45 2022
+
+@author: godof
+"""
+
+moonriver_MVRUSD = "0x9486E807Ca846fee51485c004f59C355D0f199B2"
+moonriver_DOTUSD = "0x03B64921dA75416c470dea4fC43B76B065d4bf24"
+moonriver_KSMUSD = "0x4d4C7472dDD2012587f566B13C3F7f036513B7ef"
